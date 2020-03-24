78e454299f1cc8430b38884abd84aa04923cb7e3
Jonathan Ballard <asgardschosen@gmail.com>
2020-03-24 16:26:53 -0700

    worked on Models.py

--- models.py
+++ models.py
@@ -7,27 +7,28 @@ from flask_migrate import Migrate
 
 
 # - Users Table:
-#     - some link to the OAuth user (likely their facebook email)
-#     - a one-to-many relationship between Users and Video with a backref
-#     - a one-to-one relationship between Users and Stream with backref
-# - Video Table:
-#     - the foreign key from Users
-#     - video length (so we can list it on /display page), perhaps we can get this from the video when we receive it, must study embedding
-# - Stream Table:
-#     - the foreign key from Users
+#     - OAuth UserID
 
 class Users(db.Model):
     __tablename__ = 'Users'
     id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(255))
+    last_name = db.Column(db.String(255))
+    email = db.Column(db.String(255))
     creator_name = db.Column(db.String(255))
     user_avatar = db.Column(db.String(255))     #allow user to upload 32x32 avatar, must be sanitized on upload
-    # oauth_link = db.Column(db.String(255))    #link to OAuth
+    # oauth_link = db.Column(db.String(255))    #link to OAuth UserID
     earnings_tips = db.Column(db.Float)
     earnings_donations = db.Column(db.Float)
     description = db.Column(db.Text)
-    user_image = db.Column(db.String(255))  #will be relative path to image, all images must be sanitized upon upload, being named an incremental number, perhaps pictures will be a database column and we can use ID instead
+    user_image = db.Column(db.String(255))  #retrieve from OAuth Facebook Picture, default will be path to default avatar
+    stream = db.relationship("Streams", uselist = False, backref = "creator")
+    videos = db.relationship("Videos", backref = "creator")
     created_at = db.Column(db.DateTime)
     updated_at = db.Column(db.DateTime)
+
+    def __repr__(self):
+        return "UserID: " + self.id + " Name: " + self.first_name + " " + self.last_name
 
 class Videos(db.Model):
     __tablename__ = "Videos"
@@ -37,9 +38,13 @@ class Videos(db.Model):
     earnings_tips = db.Column(db.Float)
     earnings_donations = db.Column(db.Float)
     description = db.Column(db.Text)
-    video_password = db.Column(db.String(255)) #creator code, must be input by users in order to enter
+    video_author_id = db.Column(db.Integer, db.ForeignKey("Users.id", ondelete="cascade"))
     created_at = db.Column(db.DateTime)
     updated_at = db.Column(db.DateTime)
+
+
+    def __repr__(self):
+        return "VideoID: " + self.id + " video_author_id: " + self.video_author_id
 
 class Streams(db.Model):
     __tablename__ = "Streams"
@@ -52,11 +57,12 @@ class Streams(db.Model):
     amount_watcher_seconds = db.Column(db.Time) #number of seconds that all watchers combined have spent on this stream
     description = db.Column(db.Text)
     time_running = db.Column(db.Time)
-    stream_password = db.Column(db.String(255)) #creator code, must be input by users in order to enter
+    stream_author_id = db.Column(db.Integer, db.ForeignKey("Users.id", ondelete="cascade"))
     created_at = db.Column(db.DateTime)
     updated_at = db.Column(db.DateTime)
 
-
+    def __repr__(self):
+        return "StreamID: " + self.id + " stream_author_id: " + self.stream_author_id
 
 
 
