a71774ed6b18e60b817cb3923d0ac283f56a1989
Jonathan Ballard <asgardschosen@gmail.com>
2020-03-25 14:44:11 -0700

    added user account admin, edit users, /stats

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import re
 import json
 from config import app, bcrypt, db
 from flask import Flask, flash, redirect, render_template, request, session, jsonify
-# from models import User_Character, Users, Equipment, Loadouts, Active_Abilities, Passive_Abilities, Ships, user_ships, Map
+from models import Users, FBUsers, Videos, Streams
 from sqlalchemy.sql import func
 
 
@@ -28,20 +28,79 @@ from sqlalchemy.sql import func
 
 
 #Stats Page
-
-
+@app.route("/stats")
+def statsRoute(userid):
+    thisUser = Users.query.get(session['user_id'])    #will need to check what it's actually called in session
+    return render_template('stats.html', thisUser = thisUser)
 
 
 #Create Page
+@app.route("/create")
+def createPage():
+    thisUser = Users.query.get(session['user_id'])    #will need to check what it's actually called in session
+    return render_template("create.html")
 
 
+#Admin Page
+@app.route("/admin")
+def adminPage():
+    thisUser = Users.query.get(session['user_id'])    #will need to check what it's actually called in session
+    allUsers = Users.query.all()
+    return render_template("admin.html")
 
 
+#Edit User Page
+@app.route("/editUser/<userID>")
+def editUserPage(userID):
+    thisUser = Users.query.get(session['user_id'])    #will need to check what it's actually called in session
+    editingUser = Users.query.get(userID)
+    return render_template("edituser.html", editingUser = editingUser)
 
 
+#Update User POST Route
+@app.route("/updateUser/<userID>", methods=["POST"])
+def updateUser(userID):
+    thisUser = Users.query.get(session['user_id'])    #will need to check what it's actually called in session
+    editingUser = Users.query.get(userID)
+
+    admin = request.form['admin']
+    firstName = request.form['first_name']
+    lastName = request.form['last_name']
+    email = request.form['email']
+    creatorName = request.form['creator_name']
+    earningsTips = request.form['earnings_tips']
+    earningsDonations = request.form['earnings_donations']
+    earningsTimeBased = request.form['earnings_watcher_seconds']
+    FBUserID = request.form['fb_user_id']
+
+    editingUser.admin = admin
+    editingUser.first_name = firstName
+    editingUser.last_name = lastName
+    editingUser.email = email
+    editingUser.creator_name = creatorName
+    editingUser.earnings_tips = earningsTips
+    editingUser.earnings_donations = earningsDonations
+    editingUser.earnings_watcher_seconds = earningsTimeBased
+    editingUser.fb_user_id = FBUserID
+
+    db.session.add(editingUser)
+    db.session.commit()
 
 
+    return redirect("/admin")
 
+
+#Delete User Route
+@app.route("/deleteUser/<userID>")
+def deleteUser(userID):
+    thisUser = Users.query.get(session['user_id'])    #will need to check what it's actually called in session
+    editingUser = Users.query.get(userID)
+    if thisUser.admin == True:   #make sure person trying to do this is an admin
+        db.session.delete(editingUser)
+        db.session.commit()
+        return redirect("/admin")
+    else:
+        return redirect("/")
 
 
 
